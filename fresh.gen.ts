8d21dda3ceb62bb6ea0fa8784179f102db839d8d
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-08-11 22:18:03 +0900

    remove extra routes

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,8 +5,6 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_data from "./routes/api/data.ts";
-import * as $api_joke from "./routes/api/joke.ts";
-import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Counter from "./islands/Counter.tsx";
 import type { Manifest } from "$fresh/server.ts";
@@ -16,8 +14,6 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/data.ts": $api_data,
-    "./routes/api/joke.ts": $api_joke,
-    "./routes/greet/[name].tsx": $greet_name_,
     "./routes/index.tsx": $index,
   },
   islands: {
